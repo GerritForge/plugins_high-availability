4ddb716f499d0acc855e223de47bb61aa926e679
Luca Milanesio <luca.milanesio@gmail.com>
2020-01-25 02:06:05 +0000

    Use separate executor for interactive indexing
    Gerrit has two separate executors for interactive and
    batch indexing operations, for preventing massive background
    reindexing to block the interactive operations that need
    immediate feedback.
    
    Use separate executors in the forwarding of the indexing
    events exactly in the same way Gerrit does, so that propagation
    to the secondary nodes happens in a timely manner, also in
    presence of a backlog of batch operations.
    
    Change-Id: Ic78c6e1348c54ef2f2ce9fd000d5fdf9022c2932

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/index/IndexEventHandler.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/index/IndexEventHandler.java
@@ -29,16 +29,22 @@ import java.util.concurrent.Executor;
 
 class IndexEventHandler
     implements ChangeIndexedListener, AccountIndexedListener, GroupIndexedListener {
+	private static final Logger logger = LoggerFactory.getLogger(IndexEventHandler.class);
   private final Executor executor;
+  private final Executor interactiveExecutor;
   private final Forwarder forwarder;
   private final String pluginName;
   private final Set<IndexTask> queuedTasks = Collections.newSetFromMap(new ConcurrentHashMap<>());
 
   @Inject
   IndexEventHandler(
-      @IndexExecutor Executor executor, @PluginName String pluginName, Forwarder forwarder) {
+      @IndexExecutor Executor executor,
+      @InteractiveIndexExecutor Executor interactiveExecutor,
+      @PluginName String pluginName,
+      Forwarder forwarder) {
     this.forwarder = forwarder;
     this.executor = executor;
+    this.interactiveExecutor = interactiveExecutor;
     this.pluginName = pluginName;
   }
 
@@ -76,7 +82,11 @@ class IndexEventHandler
     if (!Context.isForwardedEvent()) {
       IndexChangeTask task = new IndexChangeTask(id, deleted);
       if (queuedTasks.add(task)) {
-        executor.execute(task);
+        if (Thread.currentThread().getName().contains("Interactive")) {
+          interactiveExecutor.execute(task);
+        } else {
+          executor.execute(task);
+        }
       }
     }
   }
